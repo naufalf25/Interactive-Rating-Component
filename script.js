d5283217d070b3ab8d41bb28286c00c21b3d1f99
naufalf25 <99015050+naufalf25@users.noreply.github.com>
2022-03-25 18:10:33 +0700

    Update commit

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-var submit = document.getElementById('submit');
-
-submit.onclick = function() {
-    function insertCss( code ) {
-        var style = document.createElement('style');
-        style.type = 'text/css';
-        if (style.styleSheet) {
-            // IE
-            style.styleSheet.cssText = code;
-        } else {
-            // Other browsers
-            style.innerHTML = code;
+function submit() {
+    if (document.getElementById('one').checked || document.getElementById('two').checked || document.getElementById('three').checked || document.getElementById('four').checked || document.getElementById('five').checked) {
+        function insertCss( code ) {
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            if (style.styleSheet) {
+                style.styleSheet.cssText = code;
+            } else {
+                style.innerHTML = code;
+            }
+            document.getElementsByTagName('head')[0].appendChild( style );
         }
-        document.getElementsByTagName('head')[0].appendChild( style );
+        insertCss(
+            ".rating-section {display: none;}" +
+            ".thankyou-section {display: unset}"
+        )
+        
+        var rate_value;
+        if (document.getElementById('one').checked) {
+            rate_value = document.getElementById('one').value;
+        } else if (document.getElementById('two').checked) {
+            rate_value = document.getElementById('two').value;
+        } else if (document.getElementById('three').checked) {
+            rate_value = document.getElementById('three').value;
+        } else if (document.getElementById('four').checked) {
+            rate_value = document.getElementById('four').value;
+        } else if (document.getElementById('five').checked) {
+            rate_value = document.getElementById('five').value;
+        }
+        document.getElementById("rating-user").innerHTML = rate_value;
+    } else {
+        document.getElementById('alert').innerHTML = "Please select one rating!";
     }
-    insertCss(
-        ".rating-section {display: none;}" +
-        ".thankyou-section {display: unset}"
-    )
-    
-    var rate_value;
-    if (document.getElementById('one').checked) {
-        rate_value = document.getElementById('one').value;
-    } else if (document.getElementById('two').checked) {
-        rate_value = document.getElementById('two').value;
-    } else if (document.getElementById('three').checked) {
-        rate_value = document.getElementById('three').value;
-    } else if (document.getElementById('four').checked) {
-        rate_value = document.getElementById('four').value;
-    } else if (document.getElementById('five').checked) {
-        rate_value = document.getElementById('five').value;
-    }
-    
-    document.getElementById("rating-user").innerHTML = rate_value;
 }
